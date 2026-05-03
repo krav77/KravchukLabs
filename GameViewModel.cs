731585000918d179e43653200d27f3e8f7b494d2
krav77 <kravnika12@gmail.com>
2026-05-03 18:33:41 +0300

    Фінальна версія.

---
+++ GameViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Labs
+{
+    public class GameViewModel : INotifyPropertyChanged
+    {
+        private string[] _board = new string[9];
+        private bool _xTurn = true;
+        private string _statusText = "";
+        private bool _gameOver = false;
+        private int _scoreX, _scoreO, _draws;
+
+        public string StatusText { get { return _statusText; } private set { _statusText = value; OnPropertyChanged(); } }
+        public int ScoreX { get { return _scoreX; } private set { _scoreX = value; OnPropertyChanged(); } }
+        public int ScoreO { get { return _scoreO; } private set { _scoreO = value; OnPropertyChanged(); } }
+        public int Draws { get { return _draws; } private set { _draws = value; OnPropertyChanged(); } }
+
+        public string C0 { get { return _board[0]; } }
+        public string C1 { get { return _board[1]; } }
+        public string C2 { get { return _board[2]; } }
+        public string C3 { get { return _board[3]; } }
+        public string C4 { get { return _board[4]; } }
+        public string C5 { get { return _board[5]; } }
+        public string C6 { get { return _board[6]; } }
+        public string C7 { get { return _board[7]; } }
+        public string C8 { get { return _board[8]; } }
+
+        public ICommand MakeMoveCommand { get; }
+        public ICommand RestartCommand { get; }
+
+        public GameViewModel()
+        {
+            MakeMoveCommand = new RelayCommand(
+                p => MakeMove(int.Parse(p.ToString())),
+                p => p != null && !_gameOver && _board[int.Parse(p.ToString())] == ""
+            );
+            RestartCommand = new RelayCommand(p => Restart());
+            Restart();
+        }
+
+        private void MakeMove(int index)
+        {
+            _board[index] = _xTurn ? "X" : "O";
+            NotifyCells();
+
+            string winner;
+            if (CheckWinner(out winner))
+            {
+                StatusText = "Переміг гравець " + winner + "!";
+                _gameOver = true;
+                if (winner == "X") ScoreX++; else ScoreO++;
+            }
+            else if (IsDraw())
+            {
+                StatusText = "Нічия!";
+                _gameOver = true;
+                Draws++;
+            }
+            else
+            {
+                _xTurn = !_xTurn;
+                StatusText = "Хід гравця " + (_xTurn ? "X" : "O");
+            }
+        }
+
+        private bool CheckWinner(out string winner)
+        {
+            int[][] lines = {
+                new[]{0,1,2}, new[]{3,4,5}, new[]{6,7,8},
+                new[]{0,3,6}, new[]{1,4,7}, new[]{2,5,8},
+                new[]{0,4,8}, new[]{2,4,6}
+            };
+            foreach (var line in lines)
+            {
+                string a = _board[line[0]], b = _board[line[1]], c = _board[line[2]];
+                if (a != "" && a == b && b == c) { winner = a; return true; }
+            }
+            winner = ""; return false;
+        }
+
+        private bool IsDraw()
+        {
+            foreach (var cell in _board) if (cell == "") return false;
+            return true;
+        }
+
+        private void Restart()
+        {
+            for (int i = 0; i < 9; i++) _board[i] = "";
+            _xTurn = true; _gameOver = false;
+            StatusText = "Хід гравця X";
+            NotifyCells();
+        }
+
+        private void NotifyCells()
+        {
+            for (int i = 0; i <= 8; i++) OnPropertyChanged("C" + i);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,99 +1,27 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Labs
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public string[,] map;
-        public string current_player;
-        int turn_number;
-        public MainWindow()
-        {
-            InitializeComponent();
+        public MainWindow() => InitializeComponent();
+    }
 
-            current_player = "X";
-            turn_number = 1;
-            map = new string[3, 3];
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    map[i, j] = " ";
-                }
-            }
+    public class SymbolColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            if (s == "X") return new SolidColorBrush(Color.FromRgb(0x4F, 0xC3, 0xF7));
+            if (s == "O") return new SolidColorBrush(Color.FromRgb(0xEF, 0x9A, 0x9A));
+            return Brushes.Transparent;
         }
 
-        private void make_move(object sender, RoutedEventArgs e)
-        {
-            Button pressed_button = (Button)sender;
-            string pressed_button_name = pressed_button.Name;
-
-            bool correct = false;
-            while (correct != true)
-            {
-                int player_row = int.Parse(pressed_button_name[1].ToString());
-                int player_col = int.Parse(pressed_button_name[2].ToString());
-                if (player_row < 0 || player_row > 2 || player_col < 0 || player_col > 2)
-                {
-                    MessageBox.Show("Некоректне введення! Введiть повторно.");
-                }
-                else if (map[player_row, player_col] != " ")
-                {
-                    MessageBox.Show("Некоректне введення! Тут вже заповнено. Введiть повторно.");
-                }
-                else
-                {
-                    correct = true;
-                    map[player_row, player_col] = current_player;
-                    pressed_button.Content = current_player;
-                }
-            }
-
-            check_win();
-
-            if (current_player == "X")
-            {
-                current_player = "O";
-            }
-            else if (current_player == "O")
-            {
-                current_player = "X";
-            }
-
-            if (turn_number == 9)
-            {
-                MessageBox.Show("Нічия!");
-                try
-                {
-                    Application.Current.Shutdown();
-                }
-                catch { }
-            }
-            turn_number++;
-        }
-
-        private void check_win()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                if ((map[i, 0] == map[i, 1] && map[i, 1] == map[i, 2] && map[i, 2] == current_player) || // Вертикаль
-                    (map[0, i] == map[1, i] && map[1, i] == map[2, i] && map[2, i] == current_player) || // Горизонталь
-                    (map[0, 0] == map[1, 1] && map[1, 1] == map[2, 2] && map[2, 2] == current_player) || // Головна діагональ
-                    (map[0, 2] == map[1, 1] && map[1, 1] == map[2, 0] && map[2, 0] == current_player))   // Побічна діагональ
-                {
-
-                    MessageBox.Show(current_player + " виграли!");
-                    try
-                    {
-                        Application.Current.Shutdown();
-                    }
-                    catch { }
-                }
-            }
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { throw new NotImplementedException(); }
     }
 }
